50c19a3f11df59f65001ad0d02d7a49de0b68358
Sanjay Choudhary <96792511+cu-sanjay@users.noreply.github.com>
2022-06-29 01:49:51 +0530

    Updated R Studio Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,30 @@
-# WEEK 3 ASSIGNMENT SANJAY CHOUDHARY
+## WEEK 3 ASSIGNMENT SANJAY CHOUDHARY
+
+makeCacheMatrix <- function(x = matrix()) {
+  a <- NULL
+  set <- function(y) {
+    x <<- y
+    a <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) a <<- inverse
+  getinverse <- function() a
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## SANJAY CHOUDHARY
+
+cacheinverse <- function(x, ...) {
+  a <- x$getinverse()
+  if(!is.null(a)) {
+    message("getting cached data")
+    return(a)
+  }
+  b <- x$get()
+  a <- solve(b, ...)
+  x$setinverse(a)
+  a
+}
